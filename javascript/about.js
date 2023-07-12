a0f362cde935cfa8f4e0c8d192524cac9bc7c4cd
Vishul_Ubuntu <sinha.vishul244@gmail.com>
2023-07-12 19:02:33 +0530

    update : added animation to info page

--- javascript/about.js
+++ javascript/about.js
@@ -3,3 +3,15 @@ import { ClosePreloader } from "./preloader.js";
 window.addEventListener("load", () => {
     ClosePreloader();
 })
+
+const timeline = gsap.timeline();
+
+timeline.from('.mainlogo', {
+    opacity: 0,
+    y: "-4rem",
+    duration: 1
+}).from(".topic", {
+    opacity: 0,
+    y: "4rem",
+    duration: 1
+})
